e194bb78eeb7629bf65059c5a06d3e099ea8f90a
takeshihashimoto <takeshi@comix.co.jp>
2026-07-05 02:18:34 +0900

    full site preview: all 13 pages

--- site/assets/js/common.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  var toggle = document.getElementById('navToggle');
-  var nav = document.getElementById('gnav');
-  toggle.addEventListener('click', function(){
-    var open = nav.classList.toggle('is-open');
-    toggle.setAttribute('aria-expanded', open ? 'true' : 'false');
-  });
-})();
